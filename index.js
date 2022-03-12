fc88143773c317714f8a31e3d094ccbfc618433b
TrevorJohnson <trevor.assetsolutions@gmail.com>
2022-03-11 17:36:33 -0700

    got tests working

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let num1 = 2;
 let num2 = 31;
 let multiply = num1 * num2;
 
-let random = Math.floor(Math.random() * 10);
+let random = Math.floor(Math.random() * 10) + 1;
 
 let num4 = 7;
 let num3 = 67;
